1d6af0d7bd372b9ed80877e5e090f156f8a7b50f
chungdeptraivl <thanhchungrc229@gmail.com>
2024-05-30 11:25:36 +0700

    Initial Commit

---
+++ VNThread/VNThreadApp.swift
@@ -0,0 +1,17 @@
+//
+//  VNThreadApp.swift
+//  VNThread
+//
+//  Created by Apple on 5/30/24.
+//
+
+import SwiftUI
+
+@main
+struct VNThreadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
